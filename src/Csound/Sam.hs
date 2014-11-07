5babc2bb2e7bdf70265d51ce246e498b44e227d0
anton-k <anton.kholomiov@gmail.com>
2014-11-07 15:56:47 +0300

    open guts

--- src/Csound/Sam.hs
+++ src/Csound/Sam.hs
@@ -29,49 +29,10 @@ import Control.Applicative
 import Control.Monad.Trans.Reader
 
 import Csound.Base
+import Csound.Sam.Core
 
 -- | The main type. A stereo sample.
 type Sam = Sample Sig2
-
-data Dur = Dur D | InfDur
-
--- | The Beats Per Minute measure (BPM). Almost all values are measured in BPMs.
-type Bpm = D
-
--- | The generic type for samples.
-newtype Sample a = Sam { unSam :: ReaderT Bpm SE (S a) 
-	} deriving (Functor)
-
-instance Applicative Sample where
-	pure = Sam . pure . pure
-	(Sam rf) <*> (Sam ra) = Sam $ liftA2 (<*>) rf ra
-
-data S a = S
-	{ samSig :: a
-	, samDur :: Dur 
-	} deriving (Functor)
-
-instance Applicative S where
-	pure a = S a InfDur
-	(S f df) <*> (S a da) = S (f a) $ case (df, da) of
-		(Dur durF, Dur durA) -> Dur $ maxB durF durA
-		_			     -> InfDur
-
-instance Num a => Num (Sample a) where
-	(+) = liftA2 (+)
-	(*) = liftA2 (*)
-	(-) = liftA2 (-)
-	negate = fmap negate
-	abs = fmap abs
-	signum = fmap signum
-	fromInteger = pure . fromInteger
-
-instance Fractional a => Fractional (Sample a) where
-	recip = fmap recip
-	fromRational = pure . fromRational
-
-instance SigSpace a => SigSpace (Sample a) where
-	mapSig f = fmap (mapSig f)
 
 instance RenderCsd Sam where
     renderCsdBy opt sample = renderCsdBy opt (runSam (120 * 4) sample)
